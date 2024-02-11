4c1d8256ad3f26dddac646f3773339aa6f7ceb79
madhusomanna <18madhusomanna81@gmail.com>
2024-02-11 15:15:56 +0530

    changed region

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "github-action-mybucket-123"
+    bucket = "github-action-mybucket-123-121"
     key    = "github-action-demo.tfstate"
     region = "ap-south-1"
   }
